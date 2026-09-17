eaf7745c7a232abdb96d3fe8b07c318b5004e5b7
Izzis <al@izs.web.id>
2026-09-17 15:11:52 +0700

    Bump to 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = versionCodeOverride ?: 1
-        versionName = versionNameOverride ?: "1.2.0"
+        versionName = versionNameOverride ?: "1.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
